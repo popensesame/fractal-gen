7822158ba3ce003a314ab7fcb8016f2280c92ebe
Matthew Geiger <mgeiger@unca.edu>
2017-02-23 12:19:51 -0500

    we have timers folks

--- cantor.js
+++ cantor.js
@@ -6,35 +6,48 @@
    *
    */
 
-const yDelta = 50
-const num_iterations = 8
-const initial_coords = { x1 : 0, y1 : 0, x2 : 1000, y2 : 0 }
+var yDelta = 50
+const ratio = .666
+var steps = 7
 
-function cantor (steps=num_iterations, coords=initial_coords) {
+var t = (duration) => d3.transition().duration(duration).ease(d3.easeLinear)
+
+function gasket (coords) {
+
+}
+
+
+var cantor_start = { x1 : 0, y1 : 0, x2 : 1000, y2 : 0 }
+var cantor_coords = [cantor_start]
+
+function cantor (coords) {
+  var lines = d3.select('svg.cantor').selectAll('line')
+    .data(coords)
+  
+  lines.enter()
+    .append('line')
+      .attr('stroke-width', 2)
+      .attr('stroke', 'black')
+    .merge(lines)
+      .transition(t)
+      .attr('x1', (d) => d.x1)
+      .attr('y1', (d) => d.y1)
+      .attr('x2', (d) => d.x2)
+      .attr('y2', (d) => d.y2)
+
+  coords.forEach((d) => {
+    coords.push({
+      x1: d.x1 + ratio*(d.x2-d.x1),
+      x2: d.x2,
+      y1: d.y1,
+      y2: d.y2
+    })
+    d.x2 = d.x2 - ratio*(d.x2-d.x1)
+  })
+}
+
+var timer = d3.interval(() => {
   if (steps <= 0) return
   steps--
-  make_line(coords)
-  cantor(steps, transform(coords, true))
-  cantor(steps, transform(coords, false))
-}
-
-function transform (coords, shift_right=false, shift_down=true) {
-  let x1 = coords.x1, x2 = coords.x2, y1 = coords.y1, y2 = coords.y2
-  return {
-    x1: shift_right ? x1 + (2/3)*(x2-x1) : x1,
-    y1: shift_down  ? y1 + yDelta : y1,
-    x2: shift_right ? x2 : x2 - (2/3)*(x2-x1),
-    y2: shift_down  ? y2 + yDelta : y2
-  }
-}
-
-function make_line (coords) {
-  d3.select('svg').append('line')
-    .attr('x1', coords.x1)
-    .attr('y1', coords.y1)
-    .attr('x2', coords.x2)
-    .attr('y2', coords.y2)
-    .attr('stroke-width', 2)
-    .attr('stroke', 'black')
-}
-
+  cantor(cantor_coords)
+}, 1000)
